c43fe65bedceaf7c37988d726e1dad6bb2ce0edb
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-09 23:56:16 +0530

    created state for storing time

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,32 @@ import React, { useState } from "react";
 
 export default function App() {
   const [isStart, setIsStart] = useState(false);
+
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
   const handleStart = () => {
     setIsStart(true);
   };
+
+  const handleReset=()=>{
+    setIsStart(false);
+  }
+
+  const handleInput=(e)=>{
+    const value=parseInt(e.target.value);
+    const id=e.target.id;
+    if(id==='hours'){
+      setHours(value);
+    }else if(id==='minutes'){
+      setMinutes(value);
+    }else{
+      setSeconds(value);
+    }
+  }
+
+
   return (
     <div className="App">
       <h1>Countdown Timer</h1>
@@ -14,9 +37,9 @@ export default function App() {
       {!isStart && (
         <div className=" input-container">
           <div className=" input-box">
-            <input id="hours" placeholder="HH" />
-            <input id="hours" placeholder="HH" />
-            <input id="hours" placeholder="HH" />
+            <input onChange={handleInput} id="hours" placeholder="HH" />
+            <input onChange={handleInput} id="minutes" placeholder="HH" />
+            <input onChange={handleInput} id="seconds" placeholder="HH" />
           </div>
           <button onClick={handleStart} className=" timer-button">
             Start
@@ -35,7 +58,7 @@ export default function App() {
           </div>
           <div className="action-box">
             <button className="timer-button">Pause</button>
-            <button className="timer-button">Reset</button>
+            <button className="timer-button" onClick={handleReset}>Reset</button>
           </div>
         </div>
       )}
