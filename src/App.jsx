a6fd2fecd589e9d9c563dd77ec5d485e25149c5c
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-09 23:50:12 +0530

    state added for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
+import "./App.css";
 
-import './App.css'
-
-import React from 'react'
+import React, { useState } from "react";
 
 export default function App() {
+  const [isStart, setIsStart] = useState(false);
+  const handleStart = () => {
+    setIsStart(true);
+  };
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Countdown Timer</h1>
-      <div className=' input-container'>
-        <div className=' input-box'>
-          <input id="hours" placeholder='HH' />
-          <input id="hours" placeholder='HH' />
-          <input id="hours" placeholder='HH' />
-        </div>
-        <button className=' timer-button'>
-          Start
-        </button>
-      </div>
-    </div>
-  )
-}
 
+      {!isStart && (
+        <div className=" input-container">
+          <div className=" input-box">
+            <input id="hours" placeholder="HH" />
+            <input id="hours" placeholder="HH" />
+            <input id="hours" placeholder="HH" />
+          </div>
+          <button onClick={handleStart} className=" timer-button">
+            Start
+          </button>
+        </div>
+      )}
+
+      {isStart && (
+        <div className="show-container">
+          <div className="timer-box">
+            <div>10</div>
+            <span>:</span>
+            <div>10</div>
+            <span>:</span>
+            <div>10</div>
+          </div>
+          <div className="action-box">
+            <button className="timer-button">Pause</button>
+            <button className="timer-button">Reset</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
